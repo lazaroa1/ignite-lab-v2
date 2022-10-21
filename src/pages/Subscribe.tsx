1cf1730febbf4000e03566cf464b00c5d1c1fdd3
lazaroa1 <lazaroalves36@gmail.com>
2022-10-21 15:14:11 -0300

    feature - submit component and .env creation

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -3,7 +3,7 @@ import { Logo } from "../components/Logo";
 export default function pages() {
   return (
     <div className="min-h-screen bg-blur bg-blur bg-cover bg-no-repeat flex flex-col items-center">
-      <div className="max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
+      <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
         <div className="max-w-[640px]">
           <Logo />
           <h1 className="mt-8 text-[2.5rem] leading-tight">
@@ -16,6 +16,30 @@ export default function pages() {
             mais utilizadas e com alta demanda para acessar as melhores
             oportunidades do mercado.
           </p>
+        </div>
+        <div className="p-8 bg-gray-700 border border-gray-500 rounded">
+          <strong className="text-2xl mb-6 block">
+            Inscreva-se gratuitamente
+          </strong>
+          <form action="" className="flex flex-col gap-2 w-full">
+            <input
+              className="bg-gray-900 rounded px-5 h-14"
+              type="text"
+              placeholder="Seu nome completo"
+            />
+            <input
+              className="bg-gray-900 rounded px-5 h-14"
+              type="text"
+              placeholder="Digite seu e-mail"
+            />
+
+            <button
+              type="submit"
+              className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transiction-colors"
+            >
+              Garantir minha vaga
+            </button>
+          </form>
         </div>
       </div>
       <img src="/src/assets/code-mockup.png" className="mt-10" alt="" />
